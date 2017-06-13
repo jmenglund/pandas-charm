937596204dc5314ae7afbc0b958502e8270f3596
jmenglund <jan.markus.englund@gmail.com>
2017-06-13 12:16:43 +0200

    Modify Python versions

--- setup.py
+++ setup.py
@@ -32,9 +32,8 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     keywords=['alignment', 'BioPython', 'DendroPy', 'pandas'],
 )
